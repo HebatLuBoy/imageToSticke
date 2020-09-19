4f2ba03219d7d7fa5ef35aa0986ddcdde76b695f
My Name Is Mr Twin's <36656233+HebatLuBoy@users.noreply.github.com>
2020-09-20 01:43:18 +0700

    Add files via upload

---
+++ utils/mention.js
@@ -0,0 +1,10 @@
+/**
+ * Create mention list
+ *
+ * This is Premium feature, Check premium feature at https://saweria.co/donate/HebatLuBoy or chat Author for Information.
+ *
+ * @param  {String} id - sender id
+ * @param  {String} botNumber - bot number
+ * @param  {Array} list - group members
+ * @return {Array} Return processed array
+ */
